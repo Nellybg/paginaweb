93cf4a9f1140d74706133c9f0fad5a9b20991658
Isackson99 <is.serrano@duocuc.cl>
2022-06-08 14:33:56 -0400

    Creo js para validar login y valido login.html

---
+++ Prueba1/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#SingIn').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            Contraseña: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            email: {
+                required: "Por favor ingresa correo valido"
+            },
+            Contraseña: {
+                required: "Por favor ingresa una contraseña"
+            },
+            email: "Por favor ingresa correo valido",
+            Contraseña: "Por favor ingresa una contraseña"
+        }
+    })
+})
